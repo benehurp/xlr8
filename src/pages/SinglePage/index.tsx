7d3f9577b5b8532a323aabc413b83385ef01a348
Bene-Hur Pessoa <benehurp@gmail.com>
2022-03-22 11:52:08 -0300

    Showing api contents on page

--- src/pages/SinglePage/index.tsx
+++ src/pages/SinglePage/index.tsx
@@ -1,20 +1,30 @@
 import { useApi } from 'hooks/useApi'
+import useLocalStorage from 'hooks/useLocalStorage'
 import React, { useEffect, useState } from 'react'
+import { ImageType } from './types'
 
 const SinglePage = () => {
    const api = useApi()
-   const [data, setData] = useState([])
+   const [data, setData] = useState<ImageType[]>([])
 
    useEffect(() => {
       const fetchImages = async () => {
          const response = await api.requestImages()
          setData(response)
-         return null
       }
       fetchImages()
    }, [])
 
-   return <div>Hello World!</div>
+   return (
+      <div>
+         {data.map(({ image_id, title, url }) => (
+            <div key={image_id}>
+               <p>{title}</p>
+               <img src={url}></img>
+            </div>
+         ))}
+      </div>
+   )
 }
 
 export default SinglePage
